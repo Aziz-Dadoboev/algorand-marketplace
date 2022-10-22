79e8905c66acd8be1e5e9c6574836acf76f69759
Aziz Dadoboev <aziz.bmstu@gmail.com>
2022-10-22 03:30:30 +0300

    push changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,17 @@ export const algodClient = new algosdk.Algodv2(config.algodToken, config.algodSe
 export const indexerClient = new algosdk.Indexer(config.indexerToken, config.indexerServer, config.indexerPort);
 
 export const myAlgoConnect = new MyAlgoConnect();
+// ...
+export const minRound = 21540981;
+
+// https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0002.md
+export const marketplaceNote = "tutorial-marketplace:uv1"
+
+// Maximum local storage allocation, immutable
+export const numLocalInts = 0;
+export const numLocalBytes = 0;
+// Maximum global storage allocation, immutable
+export const numGlobalInts = 2; // Global variables stored as Int: count, sold
+export const numGlobalBytes = 3; // Global variables stored as Bytes: name, description, image
+//...
+export const ALGORAND_DECIMALS = 6;
